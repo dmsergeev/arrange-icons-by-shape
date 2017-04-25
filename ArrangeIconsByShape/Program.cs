736e4aaa3fed6a7c339d205e4adbcbe078e6039b
dmsergeev <hykkuu@gmail.com>
2017-04-24 19:37:11 -0500

    Add project files.

---
+++ ArrangeIconsByShape/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ArrangeIconsByShape
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
