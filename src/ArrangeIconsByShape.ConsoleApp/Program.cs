20d709decf4fc447a813544e841df1537bd08c88
dmsergeev <hykkuu@gmail.com>
2017-04-26 19:59:16 -0500

    altered project structure

---
+++ src/ArrangeIconsByShape.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ArrangeIconsByShape.ConsoleApp
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine($"hi");
+        }
+    }
+}
